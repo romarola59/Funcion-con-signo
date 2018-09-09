ed60a9acabf826319a179a10af0c71eac8ba2ee7
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-09 19:52:39 -0300

    Agregar archivos de proyecto.

---
+++ signo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace signo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
