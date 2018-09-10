29ad0787d3e2c0523c8244a5b9ce1bfbf5c11316
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-09 22:03:47 -0300

    Programa signo con funcion

--- signo/Program.cs
+++ signo/Program.cs
@@ -2,11 +2,35 @@
 
 namespace signo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+       public static void Signo(float n)
+       {
+            //Hacemos los if para resolver los puntos a resolver, faltaria cuando es uno para decimal.
+            
+            if (n >0)
+            Console.WriteLine("1");
+            if (n == 0) 
+            Console.WriteLine("0");
+            if (n < 0) 
+            Console.WriteLine("-1");
+            
+            
+            
+        }   
+
+
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            float n;
+            
+           // en esta parte declaro las variables ,la interacion con el usuario y llamo a la funcion. 
+
+            Console.WriteLine("ingrese un numero real : ");
+            n =Convert.ToSingle( Console.ReadLine());
+            Signo(n);
+            Console.ReadKey();
         }
+        
     }
 }
